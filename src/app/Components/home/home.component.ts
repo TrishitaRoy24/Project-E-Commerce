c4932162dc907ebd9162046bd3515870bd83ceb1
TrishitaRoy24 <roytrishita24@gmail.com>
2024-10-24 21:10:12 +0530

    e-commerce home page setup 1

---
+++ src/app/Components/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonServiceService } from '../../common-service.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit {
+
+  featuredProduct: any;
+
+  constructor(private commonService: CommonServiceService){}
+
+  ngOnInit(): void {
+    this.fetchFeaturedProduct();
+  }
+
+  fetchFeaturedProduct(){
+    this.commonService.getFeaturedProduct().subscribe((d:any)=>{
+      this.featuredProduct=d;
+      console.log(this.featuredProduct);
+      
+    });
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./Components/footer/footer.component";
 import { HeaderComponent } from "./Components/header/header.component";
+import { HomeComponent } from "./Components/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
